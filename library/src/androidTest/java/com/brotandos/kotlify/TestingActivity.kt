b39530fbd8f6aa6210322cfa35795db1545aeb8e
Brotandos <brotandos@gmail.com>
2020-08-20 00:29:57 +0600

    v0.0.T: Updates versions and refactor variables

--- library/src/androidTest/java/com/brotandos/kotlify/TestingActivity.kt
+++ library/src/androidTest/java/com/brotandos/kotlify/TestingActivity.kt
@@ -1,26 +1,13 @@
 package com.brotandos.kotlify
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.brotandos.kotlify.common.viewModels
-import com.brotandos.kotlify.container.root.VRoot
-import com.brotandos.kotlify.container.root.VRootOwner
 import com.brotandos.kotlify.sandbox.TestingViewModel
 
-class TestingActivity : AppCompatActivity(), VRootOwner {
+class TestingActivity : AppCompatActivity() {
 
-    override var vRoot: VRoot<*>? = null
     private val viewModel: TestingViewModel by viewModels()
-
-    companion object {
-        fun start(context: Context) {
-            val intent = Intent(context, TestingActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            context.startActivity(intent)
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
