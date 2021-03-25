4c9017247b5d5d73eb8d3d75859dad417de0742e
Damir Asanbaev <damir9715@gmail.com>
2021-03-25 16:36:55 +0600

    refactor: Remove listener from VLabel

--- library/src/main/kotlin/com/brotandos/kotlify/element/VLabel.kt
+++ library/src/main/kotlin/com/brotandos/kotlify/element/VLabel.kt
@@ -2,20 +2,13 @@ package com.brotandos.kotlify.element
 
 import android.content.res.ColorStateList
 import android.graphics.Typeface
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.TextView
 import androidx.annotation.ColorInt
 import com.brotandos.kotlify.common.LayoutSize
 import com.jakewharton.rxrelay2.BehaviorRelay
-import io.reactivex.Observer
-import io.reactivex.android.MainThreadDisposable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.Consumer
 
 abstract class VLabel<V : TextView>(size: LayoutSize) : WidgetElement<V>(size) {
-
-    private var listener: Listener? = null
 
     var textResId: Int? = null
 
@@ -55,36 +48,12 @@ abstract class VLabel<V : TextView>(size: LayoutSize) : WidgetElement<V>(size) {
     override fun initSubscriptions(view: V?) {
         super.initSubscriptions(view)
         textResourceRelay
-                ?.observeOn(AndroidSchedulers.mainThread())
-                ?.subscribe { view?.setText(it) }
-                ?.untilLifecycleDestroy()
-        textRelay?.let { relay ->
-            view?.let { listener = Listener(it, relay).also(it::addTextChangedListener) }
-            relay
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { text ->
-                    listener?.let { view?.removeTextChangedListener(it) }
-                    view?.text = text
-                    listener?.let { view?.addTextChangedListener(it) }
-                }
-                .untilLifecycleDestroy()
-        }
-    }
-
-    private class Listener(private val view: TextView, private val observer: Consumer<in String>) :
-        MainThreadDisposable(), TextWatcher {
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = Unit
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) = Unit
-        override fun afterTextChanged(s: Editable) {
-            if (!isDisposed) {
-                view.removeTextChangedListener(this)
-                observer.accept(s.toString())
-                view.addTextChangedListener(this)
-            }
-        }
-
-        override fun onDispose() {
-            view.removeTextChangedListener(this)
-        }
+            ?.observeOn(AndroidSchedulers.mainThread())
+            ?.subscribe { view?.setText(it) }
+            ?.untilLifecycleDestroy()
+        textRelay
+            ?.observeOn(AndroidSchedulers.mainThread())
+            ?.subscribe { view?.text = it }
+            ?.untilLifecycleDestroy()
     }
 }
