c62e0c064757bb3b4d1b39eadeae3e2565c13248
Damir Asanbaev <damir9715@gmail.com>
2021-03-25 11:48:01 +0600

    feat: Add transparent background for bottomSheetDialog

--- library/src/main/kotlin/com/brotandos/kotlify/container/modal/VBottomSheetDialog.kt
+++ library/src/main/kotlin/com/brotandos/kotlify/container/modal/VBottomSheetDialog.kt
@@ -1,13 +1,14 @@
 package com.brotandos.kotlify.container.modal
 
 import android.content.Context
+import com.brotandos.kotlify.R
 import com.brotandos.kotlify.common.KotlifyContext
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class VBottomSheetDialog : ModalElement<BottomSheetDialog>() {
 
     override fun build(context: Context, kotlifyContext: KotlifyContext): BottomSheetDialog {
-        val dialog = BottomSheetDialog(context)
+        val dialog = BottomSheetDialog(context, R.style.TransparentBottomSheetDialogTheme)
         titleResId?.let(dialog::setTitle) ?: title?.let(dialog::setTitle)
         cancellable?.let(dialog::setCancelable)
         initSubscriptions(dialog)
