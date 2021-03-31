fcc9cca4a372498396df89cfadb731d7902c736c
Damir Asanbaev <damir9715@gmail.com>
2021-03-31 15:47:23 +0600

    feat: Add textChanges() extension for TextView

--- library/src/main/kotlin/com/brotandos/kotlify/element/VLabel.kt
+++ library/src/main/kotlin/com/brotandos/kotlify/element/VLabel.kt
@@ -2,10 +2,16 @@ package com.brotandos.kotlify.element
 
 import android.content.res.ColorStateList
 import android.graphics.Typeface
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.TextView
+import androidx.annotation.CheckResult
 import androidx.annotation.ColorInt
 import com.brotandos.kotlify.common.LayoutSize
 import com.jakewharton.rxrelay2.BehaviorRelay
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.MainThreadDisposable
 import io.reactivex.android.schedulers.AndroidSchedulers
 
 abstract class VLabel<V : TextView>(size: LayoutSize) : WidgetElement<V>(size) {
@@ -61,5 +67,64 @@ abstract class VLabel<V : TextView>(size: LayoutSize) : WidgetElement<V>(size) {
             ?.observeOn(AndroidSchedulers.mainThread())
             ?.subscribe { view?.inputType = it }
             ?.untilLifecycleDestroy()
+    }
+}
+
+@CheckResult
+fun TextView.textChanges(): InitialValueObservable<CharSequence> {
+    return TextViewTextChangesObservable(this)
+}
+
+private class TextViewTextChangesObservable(
+    private val view: TextView
+) : InitialValueObservable<CharSequence>() {
+
+    override fun subscribeListener(observer: Observer<in CharSequence>) {
+        val listener = Listener(view, observer)
+        observer.onSubscribe(listener)
+        view.addTextChangedListener(listener)
+    }
+
+    override val initialValue get() = view.text
+
+    private class Listener(
+        private val view: TextView,
+        private val observer: Observer<in CharSequence>
+    ) : MainThreadDisposable(), TextWatcher {
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            if (!isDisposed) {
+                observer.onNext(s)
+            }
+        }
+
+        override fun afterTextChanged(s: Editable) {
+        }
+
+        override fun onDispose() {
+            view.removeTextChangedListener(this)
+        }
+    }
+}
+
+abstract class InitialValueObservable<T> : Observable<T>() {
+    protected abstract val initialValue: T
+
+    override fun subscribeActual(observer: Observer<in T>) {
+        subscribeListener(observer)
+        observer.onNext(initialValue)
+    }
+
+    protected abstract fun subscribeListener(observer: Observer<in T>)
+
+    fun skipInitialValue(): Observable<T> = Skipped()
+
+    private inner class Skipped : Observable<T>() {
+        override fun subscribeActual(observer: Observer<in T>) {
+            subscribeListener(observer)
+        }
     }
 }
